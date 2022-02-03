d20a41a8a49e2e9a2a7836326010e06b7b324752
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-03 23:00:29 +0530

    Create addThreeNumbers.java

---
+++ addThreeNumbers.java
@@ -0,0 +1,13 @@
+package com.basicsinjava;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("The sum of all three is: ");
+        int n1 = 12;
+        int n2 = 7;
+        int n3 = 23;
+        int sum = n1+n2+n3;
+        System.out.println(sum);
+    }
+}
