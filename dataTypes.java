13e3ecfab35c5e64bf945083c1f8f0f7dc8b487f
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-03 23:23:50 +0530

    Create dataTypes.java

---
+++ dataTypes.java
@@ -0,0 +1,17 @@
+package com.basicsinjava;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte age = 12;
+        int age1 = 34;
+        short age2 = 57;
+        long ageDino = 34362576272756277L;
+        char ch = 'A';
+        float f1 = 5.6f;   // or 5.6F
+        double d = 4.78D;  // or 4.78d  or 4.78 
+        boolean a = true;
+
+        System.out.println(age);
+    }
+}
