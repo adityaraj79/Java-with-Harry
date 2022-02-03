c5280c70dba37f3f1976e13e8f1c7ff0135c446d
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-03 23:48:27 +0530

    Create float.java

---
+++ float.java
@@ -0,0 +1,15 @@
+package com.basicsinjava;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        float sum = a+b;
+        System.out.println("Sum is: "+ sum);
+        
+    }
+}
