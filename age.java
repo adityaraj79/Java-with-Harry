98fed5beddda608d9358678fdfbf69675a986366
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-06 00:13:03 +0530

    Create age.java

---
+++ age.java
@@ -0,0 +1,22 @@
+package com.basicsinjava;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if(age>56){
+            System.out.println("Experienced");
+        }else if(age>46){
+            System.out.println("Semi_experienced");
+        }else if(age>36){
+            System.out.println("Somewhat_experienced");
+        }else{
+            System.out.println("Not_experienced");
+        }if(age>5){
+            System.out.println("Not a baby");
+        }
+    }
+}
