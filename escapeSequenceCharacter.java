1d5eaa7b3318d809a0f943b088ceea3f51cc54b4
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-05 23:22:32 +0530

    Create escapeSequenceCharacter.java

---
+++ escapeSequenceCharacter.java
@@ -0,0 +1,11 @@
+package com.basicsinjava;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("I am Aditya.\"jhug");
+        
+    }
+}
