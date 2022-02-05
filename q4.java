e51eb247393f68df0658b726320b0b7eb7319e96
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-05 23:55:33 +0530

    Create q4.java

---
+++ q4.java
@@ -0,0 +1,12 @@
+package com.basicsinjava;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String letter = "Dear Aditya,\n\t I like to meet you.\n Thank you!";
+        System.out.println(letter);
+    }
+}
