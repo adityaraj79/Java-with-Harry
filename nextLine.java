32c7b7eaee2cf297f4f8da0fad56e8e27f9ef07f
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-05 22:28:18 +0530

    Create nextLine.java

---
+++ nextLine.java
@@ -0,0 +1,16 @@
+package com.basicsinjava;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        String str = sc.next();
+//        System.out.println(str);
+        // if input is this is aditya, output will be this(it will take only one token.) 
+
+        String str = sc.nextLine();
+        System.out.println(str);
+    }
+}
