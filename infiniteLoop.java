e289bc710c6b2bd4f8c9bbc09e9a01a1eb87cd37
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-08 23:42:11 +0530

    Create infiniteLoop.java

---
+++ infiniteLoop.java
@@ -0,0 +1,11 @@
+package com.basicsinjava;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        while(true){
+            System.out.println("Aditya Raj!");
+        }
+    }
+}
