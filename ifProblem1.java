0e0cec6db5077203cdeda8f4487e7e67793dd8f7
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-08 22:11:56 +0530

    Create ifProblem1.java

---
+++ ifProblem1.java
@@ -0,0 +1,13 @@
+package com.basicsinjava;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 10;
+        if(a==11){
+            System.out.println("You are 11.");
+        }else{
+            System.out.println("You are not 11.");
+        }
+    }
+}
