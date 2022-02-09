cb5af4d3ca7229460e87e88a81403688277f819c
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-09 22:51:14 +0530

    Create forQuiz.java

---
+++ forQuiz.java
@@ -0,0 +1,16 @@
+package com.basicsinjava;
+
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i<n; i++){
+            System.out.println(2*i+1);
+        }
+        
+    }
+}
