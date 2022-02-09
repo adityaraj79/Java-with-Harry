d87a1a740ef56655f465a57bbdb5ef90f4cbc035
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-09 22:26:59 +0530

    Create doWhileLoop2.java

---
+++ doWhileLoop2.java
@@ -0,0 +1,18 @@
+package com.basicsinjava;
+
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 10;
+        do {
+            System.out.println(i);
+            i++;
+            
+        } while (i<=5);
+        // this loop first print the statement and then check the condition.
+    }
+}
