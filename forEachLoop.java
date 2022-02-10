229809f61b2106cb872a504ceeb0bbec1bb8d1a2
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-10 23:25:39 +0530

    Create forEachLoop.java

---
+++ forEachLoop.java
@@ -0,0 +1,15 @@
+package com.basicsinjava;
+
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] marks ={96,95,90,83,61};
+        for(int element: marks){
+            System.out.println(element);
+        }
+    }
+}
