83123eaf53fc87028ec47ef6d53cecc7c9777e39
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-12 00:23:56 +0530

    Update arrayQn7.java

--- arrayQn7.java
+++ arrayQn7.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int [] marks = {96, 95, 91, 832, 6};
-        int min=10000;
+        int min=Integer.MAX_VALUE;
         for(int i=0; i<marks.length; i++){
             if(marks[i]<min){
                 min=marks[i];
