de7234f73c4c52d89f72a2f1cc63ad9eac939780
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-13 23:06:30 +0530

    Create pushCode.java

---
+++ pushCode.java
@@ -0,0 +1,14 @@
+package com.basicsinjava;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("The sum of all three is: ");
+        int n1 = 12;
+        int n2 = 7;
+        int n3 = 23;
+        int n4 = 34;
+        int sum = n1+n2+n3+n4;
+        System.out.println(sum);
+    }
+}
