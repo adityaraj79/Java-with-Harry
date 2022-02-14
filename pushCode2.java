3d31b34e113444ac35018e41d786af7ab7834bb0
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-14 21:55:22 +0530

    Create pushCode2.java

---
+++ pushCode2.java
@@ -0,0 +1,9 @@
+package com.basicsinjava;
+
+public class GithubPush {
+    public static void main(String[] args) {
+        for(int c=0; c<5; c++){
+            System.out.println("I will definitely start web dev after completing java!");
+        }
+    }
+}
