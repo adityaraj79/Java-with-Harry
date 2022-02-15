ea524483c9900c0bd6fa0da72fe20bcd3ef22dcd
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-15 23:41:51 +0530

    Create pushCode3.java

---
+++ pushCode3.java
@@ -0,0 +1,9 @@
+package com.basicsinjava;
+
+public class GithubPush {
+    public static void main(String[] args) {
+        for(int c=1; c<=5; c++){
+            System.out.println("I loves java!");
+        }
+    }
+}
