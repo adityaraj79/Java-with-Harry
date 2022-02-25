e02607b3268c3af5ff20b2c2293f36212f588862
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-25 23:49:47 +0530

    Create pushCode9.java

---
+++ pushCode9.java
@@ -0,0 +1,15 @@
+package com.basicsinjava;
+
+import java.util.*;
+
+public class GithubPush {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+        for(int c=0; c<=15; c++){
+            System.out.println(name);
+        }
+        System.out.println("Thanku for using this Code...");
+    }
+}        
