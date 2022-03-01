c015268da00805404450c8e021dab8ab85ce18e4
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-03-01 23:32:43 +0530

    Create pushCode10.java

---
+++ pushCode10.java
@@ -0,0 +1,10 @@
+package com.basicsinjava;
+
+public class GithubPush {
+    public static void main(String[] args) {
+        for(int c=1; c<=5; c++){
+            System.out.println("i'm Ahjdg");
+        }
+        System.out.println("Thanks for using the code...");
+    }
+}
