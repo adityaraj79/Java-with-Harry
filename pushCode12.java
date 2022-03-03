443edf1fdb8fcd75367427d67f580e301ea4726f
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-03-03 23:03:26 +0530

    Create pushCode12.java

---
+++ pushCode12.java
@@ -0,0 +1,15 @@
+package com.basicsinjava;
+
+import java.util.*;
+
+public class GithubPush {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type your name here!");
+        String name = sc.nextLine();
+        for(int c=1; c<=5; c++){
+            System.out.println(name);
+        }
+        System.out.println("Thanksm;f");
+    }
+}
