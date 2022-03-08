e9b66dac1ed30b8bc5d74e26654c0d8123c02dc3
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-03-08 22:36:57 +0530

    Create pushCode14.java

---
+++ pushCode14.java
@@ -0,0 +1,9 @@
+package com.basicsinjava;
+
+public class GithubPush {
+    public static void main(String[] args) {
+        for(int l=1; l<=5; l++){
+            System.out.println("I likes java!");
+        }
+    }
+}
