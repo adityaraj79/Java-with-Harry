84a693caeb7c239ea7265206438b10b90cfe032b
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-03-11 23:55:05 +0530

    Create pushCode17.java

---
+++ pushCode17.java
@@ -0,0 +1,16 @@
+package com.basicsinjava;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        String sr = sc.nextLine();
+//        System.out.println(sr);
+        // if input is this is aditya, output will be this(it will take only one token.) 
+
+        String str = sc.nextLine();
+        System.out.println(sr);
+    }
+}
