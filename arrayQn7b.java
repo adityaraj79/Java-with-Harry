73503367d934ff228e422fa7f69633f4dd26e81a
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-03-15 22:58:07 +0530

    Create arrayQn7b.java

---
+++ arrayQn7b.java
@@ -0,0 +1,19 @@
+package com.basicsinjava;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] marks = {96, 195, 99, 832, 6};
+        int min=Integer.MAX_VALUE;
+        for(int i=0; i<marks.length; i++){
+            if(marks[i]<min){
+                min=marks[i];
+            }
+
+        }
+        System.out.println("The minimum value of element this array is: "+min);
+    }
+}
